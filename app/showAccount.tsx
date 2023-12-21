ffe8129fc98e1512c7d78a25f6c7f56e93a98154
wantJoy1 <wantjoy1@gmail.com>
2023-12-21 16:43:08 +0900

    アカウント情報を表示するやつを追加

--- app/showAccount.tsx
+++ app/showAccount.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import { baseUrl, createRequestBody, getUrl } from "../utils/httpRequest";
+import { base, createRequest, getUrl } from "../utils/httpRequest";
 
 export default function ShowAccount() {
-  const endpoint = getUrl("i");
-  const params = createRequestBody();
+  // const endpoint = getUrl("i");
+  const endpoint = "http://192.168.13.101:30100/i";
+  const params = createRequest();
 
   const [account, setAccount] = useState("");
 
   const onButtonClick = () => {
-    console.log(endpoint.toString());
-    console.log(params);
-
-    fetch(endpoint.toString(), params)
+    fetch(endpoint, params)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -22,7 +20,7 @@ export default function ShowAccount() {
       })
       .then((resJson) => {
         console.log(resJson);
-        setAccount(JSON.stringify(resJson));
+        setAccount(JSON.stringify(resJson, null, 2));
       })
       .catch((error) => {
         console.error(error);
@@ -32,7 +30,7 @@ export default function ShowAccount() {
   return (
     <div>
       <button
-        className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+        className="align-middle select-none font-sans font-bold text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
         onClick={onButtonClick}
       >
         ShowAccount
